b7cdff4664d557a4285cd38e431da1f0bd006f94
joel <jssimola@gmail.com>
2023-09-28 15:26:46 +0300

    pp funktiot lisätty (get, add, remove)

--- game_files/Functions.py
+++ game_files/Functions.py
@@ -1,3 +1,30 @@
 def dice_roll():
     dice_num = random.randint(2, 12)
     return dice_num
+
+def get_current_pp(player_id):
+    query = f"SELECT current_pp FROM player WHERE id='{player_id}'"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchone()
+#    print(result)
+    return result
+def add_pp(change_amount, player_id):
+    current_pp = get_current_pp(player_id)
+    new_pp = int(current_pp[0]) + change_amount
+    query = f"UPDATE player SET current_pp = '{new_pp}' WHERE id='{player_id}'"
+    cursor = connection.cursor()
+    cursor.execute(query)
+#    result = cursor.fetchone()
+#    print(result)
+    return
+
+def remove_pp(change_amount, player_id):
+    current_pp = get_current_pp(player_id)
+    new_pp = int(current_pp[0]) - change_amount
+    query = f"UPDATE player SET current_pp = '{new_pp}' WHERE id='{player_id}'"
+    cursor = connection.cursor()
+    cursor.execute(query)
+#    result = cursor.fetchone()
+#    print(result)
+    return
