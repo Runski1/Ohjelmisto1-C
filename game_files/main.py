0e43daee7501f379c601a95db8759fffa56df509
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-09-28 15:49:06 +0300

    Changes to be committed: 	new file:   game_files/create_game_user.sql 	modified:   game_files/main.py

--- game_files/main.py
+++ game_files/main.py
@@ -1,5 +1,12 @@
-import functions, user_input_processor
-
+import functions, user_input_processor, mysql
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port=3306,
+         database='flight_game',
+         user='game',
+         password='pass',
+         autocommit=True
+         )
 
 
 
