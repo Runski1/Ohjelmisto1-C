4aeded65156d3f7d44c3770475c0c92376ca0ca5
Ville <villesc@gmail.com>
2023-09-29 16:13:09 +0300

    range-funktio pientä viilausta vaille redi

--- game_files/SQL_queries.py
+++ game_files/SQL_queries.py
@@ -10,7 +10,7 @@ connection = mysql.connector.connect(
          autocommit=True
          )
 
-def get_flight_route(player_id,multiply):
+def get_route(travel_mode,player_id,multiply):
     cursor = connection.cursor()
     query1 = (f"select player.current_pp,latitude_deg,longitude_deg from city inner join player on "
               f"city.id = player.location where player.id = '{player_id}'")
@@ -35,6 +35,8 @@ def get_flight_route(player_id,multiply):
     player_location = (player_data[0][1],player_data[0][2])
 
     city_in_range=[]
+
+
     #Käy läpi kaupungit ja tarkistaa voiko pelaaja matukstaa sinne
     for city in city_data:
         city_name = city[0]
@@ -42,17 +44,21 @@ def get_flight_route(player_id,multiply):
         longitude_deg = city[2]
         city_location = (latitude_deg,longitude_deg)
         distance = geodesic(player_location, city_location).kilometers
-        #testaa onko kaupunki saavutettavan matkan päässä, jos on niin se lisätään listaan
+
+        # testaa onko kaupunki saavutettavan matkan päässä, jos on niin se lisätään listaan
         if distance <= available_range:
+            if travel_mode == 'boat' and distance <=1000
+                city_in_range.append(city_name)
             city_in_range.append(city_name)
     result = city_in_range
     return result
 #force id(normaalisti tietokannasta
 player_id= 1
+#multiplieri configista
 flight_range_multiply = 1
 
 
-print(get_flight_route(player_id,flight_range_multiply))
+print(get_flight_route(travel_mode, player_id,flight_range_multiply))
 
 
         
