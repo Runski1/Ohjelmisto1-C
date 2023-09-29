c2c7d5b31f05b4442dcf52e659823968d2725024
Ville <villesc@gmail.com>
2023-09-29 15:26:33 +0300

    range-funktio pientä viilausta vaille redi

--- game_files/SQL_queries.py
+++ game_files/SQL_queries.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import geopy
+from geopy.distance import geodesic
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
@@ -10,8 +10,51 @@ connection = mysql.connector.connect(
          autocommit=True
          )
 
-def get_flight_route(player_id)
+def get_flight_route(player_id,multiply):
     cursor = connection.cursor()
-    query1 = f"select player.current_pp, latitude_deg, longitude_deg from city inner join player on city.id = player.location where player.id = '{player_id}'"
-    quert2 =     kaikki kaupungit
-    result = cursor.fetchall()
+    query1 = (f"select player.current_pp,latitude_deg,longitude_deg from city inner join player on "
+              f"city.id = player.location where player.id = '{player_id}'")
+    query2 = f"select name,latitude_deg , longitude_deg from city group by name"
+
+
+    #distance_range =  distance.great_circle(query1[1,2]),query2[1,2])
+
+    cursor.execute(query1)
+    player_data = cursor.fetchall()
+    available_range = player_data[0][0] * multiply
+    # laskee pelaajan maksimi rangen annetulla kertoimella ja nykyisellä rahan määrällä
+
+
+
+    # hakee kaikki kaupungit pelialueelta
+
+    cursor.execute(query2)
+    city_data = cursor.fetchall()
+
+    #pelaajan sijainti
+    player_location = (player_data[0][1],player_data[0][2])
+
+    city_in_range=[]
+    #Käy läpi kaupungit ja tarkistaa voiko pelaaja matukstaa sinne
+    for city in city_data:
+        city_name = city[0]
+        latitude_deg = city[1]
+        longitude_deg = city[2]
+        city_location = (latitude_deg,longitude_deg)
+        distance = geodesic(player_location, city_location).kilometers
+        #testaa onko kaupunki saavutettavan matkan päässä, jos on niin se lisätään listaan
+        if distance <= available_range:
+            city_in_range.append(city_name)
+    result = city_in_range
+    return result
+#force id(normaalisti tietokannasta
+player_id= 1
+flight_range_multiply = 1
+
+
+print(get_flight_route(player_id,flight_range_multiply))
+
+
+        
+    
+        
