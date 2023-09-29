c45579410b2033c8abb7103c13916bf67deacdcc
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-09-29 14:58:59 +0300

    Tietokanta-alustus functions.py-tiedostoon

--- game_files/functions.py
+++ game_files/functions.py
@@ -1,4 +1,5 @@
 import random
+import os
 # Matias kävi lisäämässä funktioihin sql_connection-parametrit siinä toiveissa että connectionin voi muodostaa vain
 # mainissa ja kantaa sieltä minne tarvitseekaan
 
@@ -38,5 +39,20 @@ def remove_pp(change_amount, player_id, sql_connection):
 #    print(result)
     return
 
-
+def format_database_for_new_game(sql_connection):
+    try:
+        # current working dir
+        cwd = os.getcwd()
+        # Itellä on tuo with open-syntaksi vähän ymmärryksen tavoittamattomissa
+        with open(cwd + "/create_game_db.sql", "r") as sql_file:
+            sql_queries = sql_file.read().split(";")  # Lukee filen, splittaa ;-merkistä listaksi
+        cursor = sql_connection.cursor()
+        for sql_query in sql_queries:
+            sql_query = sql_query.strip()  # Vedetään tyhjät pois
+            if sql_query:  # onko query tyhjä? -> FALSE
+                cursor.execute(sql_query)
+        sql_connection.commit()
+        return "Database formatting completed."
+    except:
+        return "Something went wrong with database formatting."
 
--- game_files/main.py
+++ game_files/main.py
@@ -8,44 +8,23 @@ import game_formatting
 connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
-    # database='kadonnut_testamentti',
+    database='kadonnut_testamentti',
     user='game',
     password='pass',
     autocommit=True
 )
 new_game_selection = input("Start new game (Y/N)").lower()
-if new_game_selection == "y":
-<<<<<<< HEAD
-    game_formatting.format(connection)
-
 cursor = connection.cursor()
-player1 = input("Nickname player 1: ")
-print(f"Player 1 is now known as {player1}.")
-sql = "INSERT INTO player SET screen_name = '" + player1 + "';"
-cursor.execute(sql)
-player2 = input("Nickname player 2: ")
-sql = "INSERT INTO player SET screen_name = '" + player2 + "';"
-print(f"Player 2 is now known as {player2}")
-=======
-    cursor = connection.cursor()
+if new_game_selection == "y":
     print(functions.format_database_for_new_game(connection))
+    cursor = connection.cursor()
     player1 = input("Nickname player 1: ")
     print(f"Player 1 is now known as {player1}.")
     sql = "INSERT INTO player SET screen_name = '" + player1 + "';"
     cursor.execute(sql)
     player2 = input("Nickname player 2: ")
     sql = "INSERT INTO player SET screen_name = '" + player2 + "';"
-    cursor.execute(sql)
     print(f"Player 2 is now known as {player2}")
-    run = True
-
-    while run:
+    cursor.execute(sql)
 
 
-else:
-    # TÄMÄ VAIN YHTEYSTESTI
-    cursor = connection.cursor()
-    cursor.execute("USE kadonnut_testamentti;")
-    cursor.execute("SELECT * FROM city;")
-    print(cursor.fetchall())
->>>>>>> 41802321c17c8c99acd394e14e1eb482ede70388
