f5fa68f6c039bfd0eb500e2d090a63a5cc13ae0c
mirohi <m_hintikka@hotmail.com>
2023-09-29 14:09:52 +0300

    eventti kiskoja päivitetty

--- game_files/main.py
+++ game_files/main.py
@@ -2,14 +2,15 @@ import functions
 import user_input_processor
 import os
 import mysql.connector
+
 connection = mysql.connector.connect(
-         host='127.0.0.1',
-         port=3306,
-         # database='kadonnut_testamentti',
-         user='game',
-         password='pass',
-         autocommit=True
-         )
+    host='127.0.0.1',
+    port=3306,
+    # database='kadonnut_testamentti',
+    user='game',
+    password='pass',
+    autocommit=True
+)
 new_game_selection = input("Start new game (Y/N)").lower()
 if new_game_selection == "y":
     print(functions.format_database_for_new_game(connection))
@@ -19,3 +20,12 @@ else:
     cursor.execute("USE kadonnut_testamentti;")
     cursor.execute("SELECT * FROM city;")
     print(cursor.fetchall())
+
+cursor = connection.cursor()
+player1 = input("Nickname player 1: ")
+print(f"Player 1 is now known as {player1}.")
+sql = "INSERT INTO player SET screen_name = '" + player1 + "';"
+cursor.execute(sql)
+player2 = input("Nickname player 2: ")
+sql = "INSERT INTO player SET screen_name = '" + player2 + "';"
+print(f"Player 2 is now known as {player2}")
