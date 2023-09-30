ba76e39e097c0fa0d9e024876fa0753f73034950
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-09-30 14:56:02 +0300

    db_connection.py added  Changes to be committed: 	new file:   game_files/db_connection.py 	modified:   game_files/functions.py 	modified:   game_files/main.py

--- game_files/functions.py
+++ game_files/functions.py
@@ -1,6 +1,6 @@
 import random
 import os
-
+from db_connection import connection
 
 # Matias kävi lisäämässä funktioihin sql_connection-parametrit siinä toiveissa että connectionin voi muodostaa vain
 # mainissa ja kantaa sieltä minne tarvitseekaan
@@ -11,68 +11,68 @@ def dice_roll():
     return dice_num
 
 
-def get_current_pp(player_id, sql_connection):
+def get_current_pp(player_id):
     query = f"SELECT current_pp FROM player WHERE id='{player_id}'"
-    cursor = sql_connection.cursor()
+    cursor = connection.cursor()
     cursor.execute(query)
     result = cursor.fetchone()
     #    print(result)
     return result
 
 
-def add_pp(change_amount, player_id, sql_connection):
-    current_pp = get_current_pp(player_id, sql_connection)
+def add_pp(change_amount, player_id):
+    current_pp = get_current_pp(player_id)
     new_pp = int(current_pp[0]) + change_amount
     query = f"UPDATE player SET current_pp = '{new_pp}' WHERE id='{player_id}'"
-    cursor = sql_connection.cursor()
+    cursor = connection.cursor()
     cursor.execute(query)
     #    result = cursor.fetchone()
     #    print(result)
     return
 
 
-def remove_pp(change_amount, player_id, sql_connection):
-    current_pp = get_current_pp(player_id, sql_connection)
+def remove_pp(change_amount, player_id):
+    current_pp = get_current_pp(player_id)
     new_pp = int(current_pp[0]) - change_amount
     query = f"UPDATE player SET current_pp = '{new_pp}' WHERE id='{player_id}'"
-    cursor = sql_connection.cursor()
+    cursor = connection.cursor()
     cursor.execute(query)
     #    result = cursor.fetchone()
     #    print(result)
     return
 
 
-def format_database_for_new_game(sql_connection):
+def format_database_for_new_game():
     try:
         # current working dir
         cwd = os.getcwd()
         # Itellä on tuo with open-syntaksi vähän ymmärryksen tavoittamattomissa
         with open(cwd + "/create_game_db.sql", "r") as sql_file:
             sql_queries = sql_file.read().split(";")  # Lukee filen, splittaa ;-merkistä listaksi
-        cursor = sql_connection.cursor()
+        cursor = connection.cursor()
         for sql_query in sql_queries:
             sql_query = sql_query.strip()  # Vedetään tyhjät pois
             if sql_query:  # onko query tyhjä? -> FALSE
                 cursor.execute(sql_query)
-        sql_connection.commit()
+        connection.commit()
         return "Database formatting completed."
     except:
         return "Something went wrong with database formatting."
 
 
-def get_location(id, sql_connection):
+def get_location(id):
     sql = "SELECT name FROM city INNER JOIN player ON city.id = player.location"
     sql += " WHERE player.id = '" + id + "';"
-    cursor = sql_connection.cursor()
+    cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
 
 
-def lock_check(id, sql_connection):
+def lock_check(id):
     sql = "SELECT lockstate FROM player"
     sql += " WHERE id = '" + id + "';"
-    cursor = sql_connection.cursor()
+    cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
     lockstate = int(result[0][0])
@@ -82,10 +82,10 @@ def lock_check(id, sql_connection):
         return lockstate
 
 
-def printer(name, player_id, sql_connection):
-    current_pp = list(get_current_pp(player_id, sql_connection))
-    current_location = get_location(player_id, sql_connection)
-    lock_status = lock_check(player_id, sql_connection)
+def printer(name, player_id):
+    current_pp = list(get_current_pp(player_id))
+    current_location = get_location(player_id)
+    lock_status = lock_check(player_id)
     print("---Player status---\n")
     print(f"Name: {name}")
     print(f"Current PP: {current_pp[0]}")
--- game_files/main.py
+++ game_files/main.py
@@ -1,21 +1,13 @@
 import functions
-from user_input_processor import manual
-import os
-import mysql.connector
+# from user_input_processor import manual
+# import os
+from db_connection import connection
 import time
 
-connection = mysql.connector.connect(
-    host='127.0.0.1',
-    port=3306,
-    database='kadonnut_testamentti',
-    user='game',
-    password='pass',
-    autocommit=True
-)
 new_game_selection = input("Start new game (Y/N)").lower()
 if new_game_selection == "y":
     cursor = connection.cursor()
-    print(functions.format_database_for_new_game(connection))
+    print(functions.format_database_for_new_game())
     sql = "INSERT INTO round_counter (counter) VALUES ('1');"
     cursor.execute(sql)
 
@@ -45,7 +37,7 @@ if new_game_selection == "y":
             if rounter % 2 == 1:
                 print(f"{player1[1]} it is your turn!\n")
                 time.sleep(1)
-                functions.printer(player1[1], str(player1[0]), connection)
+                functions.printer(player1[1], str(player1[0]))
                 time.sleep(1)
                 choice_p1 = input(f"What would you like to do? ")
                 sql = "UPDATE round_counter SET counter = counter + 1"
@@ -59,14 +51,3 @@ if new_game_selection == "y":
                 choice_p2 = input("\nWhat would you like to do? ")
                 sql = "UPDATE round_counter SET counter = counter + 1"
                 cursor.execute(sql)
-
-
-
-
-else:
-    # TÄMÄ VAIN YHTEYSTESTI
-
-    cursor = connection.cursor()
-    cursor.execute("USE kadonnut_testamentti;")
-    cursor.execute("SELECT * FROM city;")
-    print(cursor.fetchall())
