153db2390fc08d45c4ae251a708c2f9d7b95a26a
mirohi <m_hintikka@hotmail.com>
2023-10-01 19:58:35 +0300

    main päivitys

--- game_files/main.py
+++ game_files/main.py
@@ -24,23 +24,26 @@ if new_game_selection == "y":
         player_table = get_player_data_as_list()
         for player in player_table:
             round_number = get_round_number()
-            current_player = player_table[round_number-1 % 2]
+            current_player = player_table[round_number - 1 % 2]
             is_lock = lock_check(str(current_player[0]))
             print(f"{current_player[1]} it is your turn!\n")
             time.sleep(1)
 
             if is_lock == "Not locked":
-                printer(current_player[1], str(current_player[0])) #printteri kutsu
+                printer(current_player[1], str(current_player[0]))  # printteri kutsu
                 time.sleep(1)
-                choice = input("\nWhat would you like to do: ")
-                user_input_processor(choice)
+                while True:
+                    choice = input("\nWhat would you like to do: ")
+                    user_input_processor(choice)
+                    if choice == "fly" or choice == "help" or choice == "sail" or choice == "hike":
+                    elif choice == "exit":
+                        exit()
+                    elix
 
-                if choice == "exit":
-                    exit()
-                print(round_number)  # tämä testaamista varten seuraa vuoronumeroa
+                print(round_number)
+
                 add_to_round_counter()
 
             elif is_lock > 0:
                 printer(current_player[1], str(current_player[0]))
                 lock_roll = dice_roll()
-
