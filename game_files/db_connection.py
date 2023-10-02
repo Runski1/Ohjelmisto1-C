f9fabcc38a0eb9de975c750bf4cd23856e859e4b
joel <jssimola@gmail.com>
2023-10-02 14:47:19 +0300

    oma modifikaatio

--- game_files/db_connection.py
+++ game_files/db_connection.py
@@ -3,7 +3,7 @@ connection = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     database='kadonnut_testamentti',
-    user='game',
-    password='pass',
+    user='root',
+    password='MariaDB',
     autocommit=True
 )
