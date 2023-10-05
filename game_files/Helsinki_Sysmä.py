89de04461e1b4c1a52ec8e4a6313e596cc11e5cf
VilleSchulz <villemsc@metropolia.fi>
2023-10-05 14:43:55 +0300

    helsinki sysmää päivitetty

--- game_files/Helsinki_Sysmä.py
+++ game_files/Helsinki_Sysmä.py
@@ -32,24 +32,23 @@ def helsinki_sysma():
             sys.stdout.write('\r' + ' ' * len(text) + '\r')  # Hide the text
             sys.stdout.flush()
             time.sleep(0.5)
-helsinki_sysma()
-input(f"{Fore.BLUE}You reached Dungo-driver! Roll dice while waiting him to arrive!{Fore.RED} Press enter!{Fore.BLUE}")
-num_roll = dice_roll()
-print(f"{Fore.GREEN}{num_roll}")
-input(f"{Fore.BLUE}You have to guide route for your loyal Dungo-driver. If you get same number you rolled before\n"
+            input(f"{Fore.BLUE}You reached Dungo-driver! Roll dice while waiting him to arrive!{Fore.RED} Press enter!{Fore.BLUE}")
+            num_roll = dice_roll()
+            print(f"{Fore.GREEN}{num_roll}")
+            input(f"{Fore.BLUE}You have to guide route for your loyal Dungo-driver. If you get same number you rolled before\n"
             f"you will get finally back to your lovely grandma and you get your name on testament! {Fore.RED}Press enter!")
-num_roll2 = dice_roll()
-print(f"{Fore.GREEN}{num_roll2}")
-lost_cities = ("Lohja","Korso","Kerava","Jyväskylä","Pieksämäki","Mellunmäki","Itäkeskus")
+            num_roll2 = dice_roll()
+            print(f"{Fore.GREEN}{num_roll2}")
+            lost_cities = ("Lohja","Korso","Kerava","Jyväskylä","Pieksämäki","Mellunmäki","Itäkeskus")
 
 
-time.sleep(1.0)
-if num_roll == num_roll2:
-    print("win")
-else:
-    print(f"{Fore.BLUE}Your not so loyal Dungo-driver got lost despite of your guiding:´(\n"
-          f"You have arrived to {Fore.YELLOW}{random.choice(lost_cities)}\n\n"
-          f"{Fore.RED}You get angry and your driver gets you back to Helsinki!!!")
+            time.sleep(1.0)
+            if num_roll == num_roll2:
+                print("win")
+            else:
+                print(f"{Fore.BLUE}Your not so loyal Dungo-driver got lost despite of your guiding:´(\n"
+                      f"You have arrived to {Fore.YELLOW}{random.choice(lost_cities)}\n\n"
+                    f"{Fore.RED}You get angry and your driver gets you back to Helsinki!!!")
 
 
 
