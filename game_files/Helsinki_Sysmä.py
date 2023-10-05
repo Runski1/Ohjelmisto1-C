4fd56c7893e4c7b0dfa1ba1ff36d3f266d4e3bcb
VilleSchulz <villemsc@metropolia.fi>
2023-10-05 12:53:40 +0300

    lisätty tekstiä introon

--- game_files/Helsinki_Sysmä.py
+++ game_files/Helsinki_Sysmä.py
@@ -3,12 +3,12 @@ from db_connection import connection
 #after_end_game
 def ending_check(helsinki_sysmä,player)
     cursor = connection.cursor()
-    sql = ("SELECT PLAYER.id, PLAYER.location, PLAYER.prizeholder FROM player INNER JOIN city ON player.location = city.id "
+    sql = ("SELECT PLAYER.id FROM player INNER JOIN city ON player.location = city.id "
            "WHERE city.NAME = 'Helsinki' and PLAYER.prizeholder= '1';")
     cursor.execute(sql)
     result = cursor.fetchall()
 
-    if result[0][0:] == player: #Jos pelaajan id on listassa löytyneiden pelaajien id:en joukossa, joilla on pääpalkinto
+    if player in result: #Jos pelaajan id on listassa löytyneiden pelaajien id:en joukossa, joilla on pääpalkinto
         # niin loppu event käynnnistyy
         helsinki_sysmä()
     else:
