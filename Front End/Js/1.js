fe8a0b4ef9d2f4132701057b8f5f707f2cf8c2c3
Ville <villesc@gmail.com>
2023-11-30 14:32:12 +0200

    front endii alettu tekee

--- Front End/Js/1.js
+++ Front End/Js/1.js
@@ -1,10 +1,20 @@
-
-
 'use strict';
 const targetElem = document.body;
-targetElem.createElement('img') ;
-targetElem.appendChild(logo);
-logo.src('./img/logo.png');
+const container = document.createElement('div');
+container.id = 'container'
+targetElem.appendChild(container);
 
+const logo = document.createElement('img');
+container.appendChild(logo);
+logo.src = '../img/logo.png';
+const enterCont = document.createElement('div')
+enterCont.id='enterGame'
+targetElem.appendChild(enterCont)
+const enterGame = document.createElement('button')
+enterGame.innerText = 'Enter the game'
+enterGame.id = 'enterGameButton';
+setTimeout(() => {
+    enterCont.appendChild(enterGame);
+}, 1000);
 
 
