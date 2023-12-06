5d25fef728db85c206e52a3c1ecacd84dc91c6be
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-12-06 13:19:14 +0200

    flask tweakers

--- Front_End/Flask/Flask.py
+++ Front_End/Flask/Flask.py
@@ -15,7 +15,7 @@ connection = mysql.connector.connect(
 server = Flask(__name__)
 CORS(server)
 
-
+# get(https://url.to.backend/get_saveGame/name_of_my_game/)
 @server.route('/get_saveGame/<savegame>')
 def get_savegame(savegame):
     sql_result = {"gameName": 'testgame', 'players': {'player1': 'ville', 'player2': 'jari'}}
@@ -26,31 +26,31 @@ def get_savegame(savegame):
     else:
         response_data = {"gameName":"not found"}
         status_code = 200
-        classes.Game(sql_result['gameName'], sql_result['players'])
+        # classes.Game(sql_result['gameName'], sql_result['players'])
 
 
-    '''cursor = connection.cursor()
-    cursor.execute(f"select name, playerName from savedGames where name = '{savegame}'")
-    sql_result = cursor.fetchone()
-    if sql_result:
-        response_data = {"gameName": f'{sql_result[0]}', "players": [f'{sql_result[1]}']}
-        status_code = 200
-    else:
-        cursor.execute("INSERT INTO savedGames (name) VALUES (%s)", (savegame,))
-        response_data = {"gameName": "not found"}
-        status_code = 400'''
+    # '''cursor = connection.cursor()
+    # cursor.execute(f"select name, playerName from savedGames where name = '{savegame}'")
+    # sql_result = cursor.fetchone()
+    # if sql_result:
+    #     response_data = {"gameName": f'{sql_result[0]}', "players": [f'{sql_result[1]}']}
+    #     status_code = 200
+    # else:
+    #     cursor.execute("INSERT INTO savedGames (name) VALUES (%s)", (savegame,))
+    #     response_data = {"gameName": "not found"}
+    #     status_code = 400'''
     response_data = json.dumps(response_data)
     response = Response(response=response_data, status=status_code, mimetype="application/json")
 
     return response
 
 
-'''@server.route('/add_player/<savegame>/<player1>/<player2>/<player3>/<player4>')
-def add_player(savegame, player):
+@server.route('/add_player/<gamename>/<player1>/<player2>/')
+def create_game(gamename, player1, player2):
     cursor = connection.cursor()
     cursor.execute("
     connection.commit()
-    cursor.close()'''
+    cursor.close()
 
 '''@server.route('/get_playerData/<savedGame>/<name>')
 def get_playerData(name):
