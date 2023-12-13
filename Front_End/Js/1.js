d09dc5087e6897bfb2a3b17c9f5716964f729ea1
VilleSchulz <villemsc@metropolia.fi>
2023-12-13 09:32:33 +0200

    vaihettu intron aikaa 56 sek

--- Front_End/Js/1.js
+++ Front_End/Js/1.js
@@ -105,7 +105,7 @@ function starTrek() {// Create container for the start screen
                 }, 3000);
             }, 4000);
         }
-    }, 54000);
+    }, 56000);
 }
 
 async function selectGame() {
