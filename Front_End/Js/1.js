0bd30c65022f622345314812e90c2614200ff038
Ville <villesc@gmail.com>
2023-12-14 19:37:15 +0200

    alku intron aika muutettu 58 sekkaa niin wörkkii paremmi

--- Front_End/Js/1.js
+++ Front_End/Js/1.js
@@ -105,7 +105,7 @@ function starTrek() {// Create container for the start screen
                 }, 3000);
             }, 4000);
         }
-    }, 43000);
+    }, 58000);
 }
 
 async function selectGame() {
